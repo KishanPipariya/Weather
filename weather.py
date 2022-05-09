f7dab939df2a4dbcd6b42b27aac3e941f6652202
Kishan Pipariya <intelkishan@gmail.com>
2022-05-09 22:24:44 +0530

    Current weather feature is implemented

--- weather.py
+++ weather.py
@@ -2,9 +2,25 @@ import config
 import requests
 import json
 
-api_key = config.api_key
-city_name = input()
-response = requests.request('GET', f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}')
-output = response.json()
-a = json.dumps(output)
-print(a)
+
+class Api:
+
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def current_weather(self, *args):
+        if len(args) == 1:
+            response = requests.request('GET', f'https://api.openweathermap.org/data/2.5/weather?q={args[0]}&appid={self.api_key}')
+            output = response.json()
+            out = json.dumps(output)
+            print(out)
+        else:
+            response = requests.request('GET', f'https://api.openweathermap.org/data/2.5/weather?lat={args[0]}&lon={args[1]}&appid={self.api_key}')
+            output = response.json()
+            out = json.dumps(output)
+            print(out)
+
+
+api = Api(config.api_key)
+api.current_weather('rajkot')
+api.current_weather('22.3039', '70.8022')
